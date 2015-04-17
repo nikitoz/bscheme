f9a60bf2e9d8499c55639c41fedbf83486dbb759
nikitoz <nikita.null@gmail.com>
2015-04-17 21:20:29 +0300

    refactoring

--- test.scm
+++ test.scm
@@ -2,4 +2,6 @@
 (define (sum a b) (+ a b))
 (define (square a) (* a a))
 
-(sum (sum 2 1) (car (cons 2 3)))
+(if (< 1 (sum (sum 2 1) (car (cons 2 3))))
+	(cons (car (cons 1 2)) (square 2))
+	(sum (square 2) (square 3)))
